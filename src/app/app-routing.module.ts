d0ea73736c1b5966f049bee88ef375a1c5e1581b
nova-sangeeth <novasangeeth@outlook.com>
2020-09-13 13:54:19 +0530

    imported the app components and added routes to them.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { Todo } from './todos/models/todo';
+import { TodosComponent } from './todos/todos.component'
+import { AboutComponent } from './about/about.component'
+const routes: Routes = [
+  { path: "", component: TodosComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
